828e1cca6112d4926df6762f825ed9b55600312b
askariya <arunalukari@gmail.com>
2020-11-14 14:59:37 -0500

    Add parallax background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import './App.css';
-import Navigation from './components/Navigation'
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navigation from './components/Navigation';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Home from './components/pages/Home'
-import Projects from './components/pages/Projects'
-import About from './components/pages/About'
-import Contact from './components/pages/Contact'
+import Home from './components/pages/Home';
+import Projects from './components/pages/Projects';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
+  const [offsetY, setoffsetY] = useState(0);
+  const handleScroll = () => setoffsetY(window.pageYOffset);
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const renderContent = () => (
     <>
-      <Router>
-        <Navigation />
-        <Switch>
-          {/* the exact keyword prevents cconfusion with other links that extend from this path*/}
-          <Route path='/' exact component={Home} />
-          <Route path='/projects' component={Projects} />
-          <Route path='/about' component={About} />
-          <Route path='/contact' component={Contact} />
-        </Switch>
-      </Router>
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+      </Switch>
+    </Router>
     </>
+    
+  );
+
+  return (
+      <>
+        <section className="Parallax">
+          
+          <div 
+            className="Parallax-background" 
+            style={{ transform: `translateY(${offsetY * 0.5}px)`}}
+          />
+          {/* <div className="Parallax-background-triangles"/> */}
+          <div className="Parallax-content">{renderContent()}</div>
+        </section>
+      </>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,6 +6,23 @@ import Hero from '../Hero';
 function Home () {
   return (
     <>
+      <div className="Parallax-content-heading">
+        <h1 className="Parallax-content-heading-text">Test</h1>
+        <h2 className="Parallax-content-heading-caption">
+          Your one-stop source of Web Development tricks
+        </h2>
+      </div>
+      <div className="Parallax-content-cta">
+        <p>
+          <b>1. Like the video.</b> Because it helps me and my channel
+        </p>
+        <p>
+          <b>2. Like the video.</b> To see more content like that!
+        </p>
+        <p>
+          <b>3. Follow the Github link.</b> And play with this code yourself!
+        </p>
+      </div>
       <Hero />
     </>
   );
