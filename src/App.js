0f1b9324d4f622501f75efc96ea3f87bc7ebe153
askariya <arunalukari@gmail.com>
2020-11-15 12:29:41 -0500

    Add incomplete functionality for background state var

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navigation from './components/Navigation';
+import React, { useState, useEffect } from "react";
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './components/pages/Home';
@@ -7,7 +8,7 @@ import Projects from './components/pages/Projects';
 import About from './components/pages/About';
 import Contact from './components/pages/Contact';
 
-import React, { useState, useEffect } from "react";
+// import ReactCSSTransitionGroup from 'react-transition-group'; // ES6
 
 function App() {
   const [offsetY, setoffsetY] = useState(0);
@@ -18,10 +19,25 @@ function App() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const [bGroundImage, setBGroundImage] = useState(() => { 
+    console.log("run App useState");
+    
+    return "images/test-para.jpg"
+  
+  });
+
+  // why doesn't this work?!?!?!
+  function setBackground(url) {
+    setBGroundImage(url);
+    console.log(document.getElementsByClassName("Parallax-background")[0].style.backgroundImage);
+    // document.getElementsByClassName("Parallax-background")[0].style.backgroundImage = "url('images/test-para.jpg')";
+    // console.log(document.getElementsByClassName("Parallax-background")[0].style.backgroundImage);
+  }
+
   const renderContent = () => (
     <>
     <Router>
-      <Navigation />
+      <Navigation onChangeTab={setBackground} />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/projects' component={Projects} />
@@ -35,13 +51,15 @@ function App() {
 
   return (
       <>
+        <span>{bGroundImage}</span>
+        <button onClick={() => setBackground("images/hotel.jpg")}> CLICK ME </button>
         <section className="Parallax">
           
           <div 
             className="Parallax-background" 
-            style={{ transform: `translateY(${offsetY * 0.5}px)`}}
+            style={{ transform: `translateY(-${offsetY * 0.5}px)`}}
           />
-          {/* <div className="Parallax-background-triangles"/> */}
+          {/*TODO find an image that can reverse move here <div className="Parallax-background-triangles"/> */}
           <div className="Parallax-content">{renderContent()}</div>
         </section>
       </>
