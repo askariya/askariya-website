2d719f5fba1759f00731b9c79f4bd68c90d5172f
askariya <arunalukari@gmail.com>
2021-04-24 12:23:00 -0400

    background-position: center;

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -20,7 +20,17 @@ function Projects () {
           </p>
         </div>
         <div className="text-block">
-          hello my dude, here is a description of the specific project
+          <p>
+            BASA is a DBMS that was created to deal with a mock-airline's basic transactions in a reliable and logical manner.
+          </p>
+          <p> 
+            It was implemented as a website to provide a user interface for the different types of users that need to access and 
+            alter information concerning the airline and its components.
+          </p> 
+          <br/>
+          <p> 
+            Click on this tile to find out more!
+          </p> 
         </div>
       </div>
       <div className="Parallax-content-cta-proj">
@@ -30,7 +40,17 @@ function Projects () {
           </p>
         </div>
         <div className="text-block">
-          hello my dude, here is a description of the specific project
+          <p>
+            uCook is a prototype for a cooking instructor app that allows you to cook multiple recipes in parallel.
+          </p> 
+          <p>
+            It's a prototype in the sense that there is very little back-end logic to the functionality it provides, 
+            rather it's a demonstration of a UI for a potential user based on researched use-cases.
+          </p> 
+          <br/>
+          <p> 
+            Click on this tile to find out more!
+          </p> 
         </div>
       </div>
       <div className="Parallax-content-cta-proj">
@@ -40,7 +60,18 @@ function Projects () {
           </p>
         </div>
         <div className="text-block">
-          hello my dude, here is a description of the specific project
+          <p>
+            Pian.io is a single-page entertainment website where users can interact with a piano keyboard to create simple music compositions. 
+            The interface is designed to be usable for both skilled musicians and novices alike.
+          </p>
+          <p>
+            While users with sufficient music knowledge will be able to use the website without any issue, novices will also be able to 
+            appreciate the simplistic interface and ease of use.
+          </p>
+          <br/>
+          <p> 
+            Click on this tile to find out more!
+          </p> 
         </div>
       </div>
     </>
