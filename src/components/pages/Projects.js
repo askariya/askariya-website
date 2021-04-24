b806b3154fbf7a86dc2a1aa942d92623d47b3847
askariya <arunalukari@gmail.com>
2021-04-24 12:59:37 -0400

    Reduce size of project cards

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -65,7 +65,7 @@ function Projects () {
             The interface is designed to be usable for both skilled musicians and novices alike.
           </p>
           <p>
-            Users will be able to create music through an online platform and collaborate with each other to generate musical compositions.
+            Users are able to create music through an online platform and collaborate with each other to generate musical compositions.
           </p>
           <br/>
           <p> 
